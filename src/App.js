724012502808ddc6e221f6f000b282a43aecb4d9
Nour ElShorbagy <nourelshorbagyy@gmail.com>
2022-03-13 16:38:53 +0200

    nevermind changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Btn from './components/Btn';
-import { Button } from '@material-ui/core';
 import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-   <LoginPage/>
+    <LoginPage/>
   );
 }
 
