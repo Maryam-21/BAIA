b86f5e3b5af8318e6d2bea3445319889645a103c
Maryam-21 <58235353+Maryam-21@users.noreply.github.com>
2022-03-16 19:19:11 +0200

    Added Register pop up dialogue form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,28 @@
 import { Box,Grid, Link } from '@material-ui/core'
+import Slide from '@mui/material/Slide';
 import React, {useState} from 'react'
 import TextBox from './TextBox'
 import Btn from './Btn'
-
+import RegisterForm from './RegisterForm'
 
 const LoginForm = () => {
     const [Email, setEmail] = useState('')
     const [Password, setPassword] = useState('')
     const [email_error, setEmail_error] = useState('')
     const [password_error, setPassword_error] = useState('')
+    const [openRegister, setOpenRegister] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpenRegister(true);
+    };
+    
+    const handleClose = () => {
+        setOpenRegister(false);
+    };
+
+    const Transition = React.forwardRef(function Transition(props, ref) {
+        return <Slide direction="up" ref={ref} {...props} />;
+    });
 
     const validate =() => {
         if(Email==''){
@@ -27,12 +41,17 @@ const LoginForm = () => {
 
 
   return (
-        <Box sx={{bgcolor:'text.disabled', display:"flex", width:"70%",height:'100%',p:'20px', borderRadius:5, border:'5px'}}>
+        <Box sx={{bgcolor:'text.disabled', display:"flex", width:"70%",height:'100%',p:'20px', borderRadius:5,
+         border:'5px'}}>
             <Grid container spacing={2} style={{height:'100%'}} alignContent='center'>
-                <Grid container direction="column"  item xs={12} spacing={2} style={{width:'100%'}} alignItems='center'>
+                <Grid container direction="column"  item xs={12} spacing={2} style={{width:'100%'}} 
+                alignItems='center'>
                     <Grid item style={{width:'75%'}}>
-                        <TextBox text="Email" required="true" type="text" onchange={setEmail} errormsg={email_error}/>
-                        <TextBox text="Password" required="true" type="password" onchange={setPassword} errormsg={password_error}/>
+                        <TextBox text="Email" required="true" type="email" onchange={setEmail} 
+                        errormsg={email_error}/>
+                        <TextBox text="Password" required="true" type="password" onchange={setPassword} 
+                        errormsg={password_error}/>
+                        <RegisterForm handleClose={handleClose} open={openRegister}></RegisterForm>
                     </Grid>
                 </Grid>
                 <Grid container item  xs={12} direction="column"  spacing={2} alignItems='center'>
@@ -42,7 +61,7 @@ const LoginForm = () => {
                     <Link href="#" underline="always">Reset Password?</Link>
                     <hr style={{width:'80%', backgroundColor:'black', height:'1px', border:0}}/>
                     <Grid item>
-                        <Btn color="" text="Register" size='large'></Btn>
+                        <Btn color="" text="Register" size='large' onclick={handleClickOpen}></Btn>
                     </Grid>
                 </Grid>
             </Grid>
---
+++ src/components/RegisterForm.js
@@ -0,0 +1,110 @@
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextBox from './TextBox'
+
+import React , {useState} from 'react'
+
+export const RegisterForm = ({Transition, handleClose, open}) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [companyName, setCompanyName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [name_error, setName_error] = useState('')
+    const [email_error, setEmail_error] = useState('')
+    const [password_error, setPassword_error] = useState('')
+    const [companyName_error, setCompanyName_error] = useState('')
+    const [phoneNumber_error, setPhoneNumber_error] = useState('')
+
+    const validate =() => {
+        let c = 0
+        if(name==''){
+            setName_error(('Name is Required'))
+            c++
+        }
+        else{
+            setName_error((''))
+        }
+        if(email==''){
+            setEmail_error(('Email is Required'))
+            c++
+        }
+        else{
+            setEmail_error((''))
+        }
+        if(password==''){
+            setPassword_error('Password is Required')
+            c++
+        }
+        else{
+            setPassword_error('')
+        }
+        if(companyName==''){
+            setCompanyName_error(('Company Name is Required'))
+            c++
+        }
+        else{
+            setCompanyName_error((''))
+        }
+        if(phoneNumber==''){
+            setPhoneNumber_error(('Phone Number is Required'))
+            c++
+        }
+        else{
+            setPhoneNumber_error((''))
+        }
+        if (c==0){
+            return true
+        }
+        return false
+    }
+
+    const onSubmit =() =>{ 
+        if(validate())
+            handleClose()
+        return
+    }
+    const onCancel =() =>{ 
+        setName_error((''))
+        setEmail_error((''))
+        setPassword_error('')
+        setCompanyName_error((''))
+        setPhoneNumber_error((''))
+        handleClose()
+        return
+    }
+
+  return (
+    <Dialog 
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth='sm'
+        maxWidth='sm'
+    >
+        <DialogTitle>{"Sign Up"}</DialogTitle>
+        <DialogContent>
+            <TextBox text="Name" required="true" type="text" onchange={setName} 
+            errormsg={name_error}/>
+            <TextBox text="Email" required="true" type="email" onchange={setEmail} 
+            errormsg={email_error}/>
+            <TextBox text="Password" required="true" type="password" onchange={setPassword} 
+            errormsg={password_error}/>
+            <TextBox text="Company Name" required="true" type="text" onchange={setCompanyName} 
+            errormsg={companyName_error}/>
+            <TextBox text="Phone Number" required="true" type="number" onchange={setPhoneNumber} 
+            errormsg={phoneNumber_error}/>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onSubmit}>Register</Button>
+          <Button onClick={onCancel}>Cancel</Button>
+        </DialogActions>
+    </Dialog>
+  )
+}
+export default RegisterForm
